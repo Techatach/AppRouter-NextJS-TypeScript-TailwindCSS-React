e74cc318ed34f5701e64e7b451655276e43ac282
Techatach <techatach@gmail.com>
2024-04-01 00:09:50 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,132 @@
-import Image from "next/image";
+import React from "react";
+import Home from "./home/page";
 
-export default function Home() {
+const page = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="p-12">
+      <div className="flex items-center justify-center text-center shadow-lg mb-5 bg-body rounded">
+        <div className="flex flex-col p-2 m-2">
+          <div className="text-3xl font-medium">Meet Our Team</div>
+          <div className="text-sm mx-2 md:text-xl text-stone-500">
+            Meet the team member who have contributed their designs for this
+            websites.
+          </div>
+          <div className="text-sm mx-2 md:text-xl mb-2 text-stone-500">
+            Resource person of Tailblocks communinty
+          </div>
+          <div className="md:grid flex">
+            <div className="flex items-center justify-center md:flex-row flex-col space-x-4  p-2">
+              <div className="flex-col px-6 py-2  max-w-7xl flex items-center justify-center">
+                <div className="md:h-40 h-28 w-28 md:w-40 rounded-full overflow-hidden bg-gray-200">
+                  <img
+                    src="https://source.unsplash.com/300x300/?man,businessman"
+                    alt=""
+                    className=""
+                  />
+                </div>
+
+                <div className="text-lg font-medium text-stone-600 cursor-pointer hover:text-stone-400 mt-5">
+                  อ.ดร.เตชทัช คลายโศกก์
+                </div>
+                <div className=" italic text-gray-500">
+                  การวิเคราะห์ข้อมูลขนาดใหญ่ สำหรับรัฐประศานศาสตร์
+                </div>
+                <div className=" italic text-gray-500">
+                  การเปลี่ยนแปลงและการพัฒนาองค์การภาครัฐ
+                </div>
+                <div className=" italic text-gray-500">
+                  การวางแผนและการบริหารนโยบายสาธารณะในยุคดิจิทัล
+                </div>
+              </div>
+              <div className="flex-col px-6 py-2 ">
+                <div className="md:h-40 h-28 w-28 md:w-40 rounded-full overflow-hidden bg-gray-200">
+                  <img
+                    src="https://source.unsplash.com/300x300/?boy"
+                    alt=""
+                    className=""
+                  />
+                </div>
+
+                <div className="text-lg font-medium text-stone-600 cursor-pointer hover:text-stone-400">
+                  Arkay
+                </div>
+                <div className=" italic text-gray-500">Executive Producer</div>
+              </div>
+              <div className="flex-col px-6 py-2 ">
+                <div className="md:h-40 h-28 w-28 md:w-40 rounded-full overflow-hidden bg-gray-200">
+                  <img
+                    src="https://source.unsplash.com/300x300/?girl"
+                    alt=""
+                    className=""
+                  />
+                </div>
+
+                <div className="text-lg font-medium text-stone-600 cursor-pointer hover:text-stone-400">
+                  Elina Gilbert
+                </div>
+                <div className=" italic text-gray-500">HR</div>
+              </div>
+            </div>
+            <div className="flex space-x-4  p-2 items-center justify-center md:flex-row flex-col ">
+              <div className="flex-col px-6 py-2 ">
+                <div className="md:h-40 h-28 w-28 md:w-40 rounded-full overflow-hidden bg-gray-200">
+                  <img
+                    src="https://source.unsplash.com/300x300/?kid"
+                    alt=""
+                    className=""
+                  />
+                </div>
+
+                <div className="text-lg font-medium text-stone-600 cursor-pointer hover:text-stone-400">
+                  Clay Johnson
+                </div>
+                <div className=" italic text-gray-500">Manager</div>
+              </div>
+              <div className="flex-col px-6 py-2 ">
+                <div className="md:h-40 h-28 w-28 md:w-40 rounded-full overflow-hidden bg-gray-200">
+                  <img
+                    src="https://source.unsplash.com/300x300/?girl,woman"
+                    alt=""
+                    className=""
+                  />
+                </div>
+
+                <div className="text-lg font-medium text-stone-600 cursor-pointer hover:text-stone-400">
+                  Hannah Saw
+                </div>
+                <div className=" italic text-gray-500">Marketing Producer</div>
+              </div>
+              <div className="flex-col px-6 py-2 ">
+                <div className="md:h-40 h-28 w-28 md:w-40 rounded-full overflow-hidden bg-gray-200">
+                  <img
+                    src="https://source.unsplash.com/300x300/?woman"
+                    alt=""
+                    className=""
+                  />
+                </div>
+
+                <div className="text-lg font-medium text-stone-600 cursor-pointer hover:text-stone-400">
+                  Wednesday Addams
+                </div>
+                <div className=" italic text-gray-500">Financer</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <script src="https://cdn.tailwindcss.com"></script>
+      <script src="https://use.fontawesome.com/03f8a0ebd4.js"></script>
+      <script
+        type="module"
+        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+      ></script>
+      <script
+        noModule
+        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+      ></script>
+      {<Home />}
+    </div>
   );
-}
+};
+
+export default page;
